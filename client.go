6d27f4376ec17557d64857dfff44c203d85874bc
robot303 <robot303@naver.com>
2021-07-15 09:34:19 +0900

    Change default of protocol type

--- client.go
+++ client.go
@@ -275,7 +275,7 @@ func NewGNMIDialOutClient(serverName, serverAddress string, insecure bool, skipv
 			return nil, err
 		}
 		if len(protocol) <= 0 {
-			protocol = "HFR"
+			protocol = "DEFAULT"
 		}
 	}
 
